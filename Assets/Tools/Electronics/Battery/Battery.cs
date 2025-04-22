08f6fe6ab3940791b8a929f8ce09f2c470072097
seif Eldein <101245844+seifEldein9@users.noreply.github.com>
2025-04-23 00:31:08 +0200

    Delete Assets directory

--- Assets/Tools/Electronics/Battery/Battery.cs
+++
@@ -1,9 +0,0 @@
-using UnityEngine;
-
-public class Battery : MonoBehaviour
-{
-    public float voltage = 5.0f; 
-
-    public Transform positiveTerminal;  
-    public Transform negativeTerminal;  
-}
